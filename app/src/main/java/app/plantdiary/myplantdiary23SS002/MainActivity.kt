82c70f2e2ec0724e0c9b26392f66ff1639759913
Brandan <brandan.jones@uc.edu>
2023-03-02 10:26:28 -0500

    Finally get our drop down working

--- app/src/main/java/app/plantdiary/myplantdiary23SS002/MainActivity.kt
+++ app/src/main/java/app/plantdiary/myplantdiary23SS002/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
@@ -15,11 +16,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import app.plantdiary.myplantdiary23SS002.ui.theme.MyPlantDiaryTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.window.PopupProperties
+import app.plantdiary.myplantdiary23SS002.dto.Plant
 
 class MainActivity : ComponentActivity() {
 
     // get our ViewModel from Koin
-    private val viewModel : MainViewModel by viewModel<MainViewModel>()
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,63 +34,140 @@ class MainActivity : ComponentActivity() {
             val plants by viewModel.plants.observeAsState(initial = emptyList())
             MyPlantDiaryTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxWidth()) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    SpecimenFacts(plants)
                 }
                 var foo = plants
                 var i = 1 + 1
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    var plantName by remember{mutableStateOf("")}
-    var location by remember{mutableStateOf("")}
-    var description by remember{mutableStateOf("")}
-    var datePlanted by remember{mutableStateOf("")}
-    val context = LocalContext.current
-    Column {
-        Text(text = "Hello $name!")
 
-        OutlinedTextField(
-            value = plantName,
-            onValueChange = { plantName = it },
-            label = { Text(stringResource(R.string.plantName)) }
-        )
-        OutlinedTextField(
-            value = location,
-            onValueChange = { location = it },
-            label = { Text(stringResource(R.string.location)) }
-        )
-        OutlinedTextField(
-            value = description,
-            onValueChange = { description = it },
-            label = { Text(stringResource(R.string.description)) }
-        )
-        OutlinedTextField(
-            value = datePlanted,
-            onValueChange = { datePlanted = it },
-            label = { Text(stringResource(R.string.datePlanted)) }
-        )
-        Button(
-            onClick = {
-                Toast.makeText(
-                    context,
-                    "$plantName $location $description $datePlanted",
-                    Toast.LENGTH_LONG
-                ).show()
-            },
+    @Composable
+    fun SpecimenFacts(plants : List<Plant> = ArrayList<Plant>()) {
+        var plantName by remember { mutableStateOf("") }
+        var location by remember { mutableStateOf("") }
+        var description by remember { mutableStateOf("") }
+        var datePlanted by remember { mutableStateOf("") }
+        val context = LocalContext.current
+        Column {
+            TextFieldWithDropDownUsage(dataIn = plants, "Plant Name")
+            OutlinedTextField(
+                value = location,
+                onValueChange = { location = it },
+                label = { Text(stringResource(R.string.location)) }
+            )
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text(stringResource(R.string.description)) }
+            )
+            OutlinedTextField(
+                value = datePlanted,
+                onValueChange = { datePlanted = it },
+                label = { Text(stringResource(R.string.datePlanted)) }
+            )
+            Button(
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "$plantName $location $description $datePlanted",
+                        Toast.LENGTH_LONG
+                    ).show()
+                },
 
-            ) { Text(text = "Save") }
+                ) { Text(text = "Save") }
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyPlantDiaryTheme {
-        Greeting("Android Development")
+    @Composable
+    fun TextFieldWithDropDownUsage(dataIn: List<Plant>, label: String = "", take: Int = 3) {
+        val dropDownOptions =remember {mutableStateOf(listOf<Plant>())}
+        val textFieldValue = remember {mutableStateOf(TextFieldValue())}
+        val dropDownExpanded = remember {mutableStateOf(false)}
+        fun onDropdownDismissRequest() {
+            dropDownExpanded.value = false
+        }
+
+        fun onValueChanged(value: TextFieldValue) {
+            dropDownExpanded.value = true
+            textFieldValue.value = value
+            dropDownOptions.value = dataIn.filter {
+                it.toString().startsWith(value.text) && it.toString() != value.text
+            }.take(take)
+        }
+
+        TextFieldWithDropdown(
+            modifier = Modifier.fillMaxWidth(),
+            value = textFieldValue.value,
+            setValue = ::onValueChanged,
+            onDismissRequest = ::onDropdownDismissRequest,
+            dropDownExpanded = dropDownExpanded.value,
+            list = dropDownOptions.value,
+            label = label
+        )
+
+    }
+
+
+    @Composable
+    fun TextFieldWithDropdown(
+        modifier: Modifier = Modifier,
+        value: TextFieldValue,
+        setValue: (TextFieldValue) -> Unit,
+        onDismissRequest: () -> Unit,
+        dropDownExpanded: Boolean,
+        list: List<Plant>,
+        label: String = "Plant Name"
+    ) {
+        Box(modifier) {
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusChanged { focusState ->
+                        if (!focusState.isFocused)
+                            onDismissRequest()
+                    },
+                value = value,
+                onValueChange = setValue,
+                label = { Text(label) },
+                colors = TextFieldDefaults.outlinedTextFieldColors()
+            )
+            DropdownMenu(
+                expanded = dropDownExpanded,
+                properties = PopupProperties(
+                    focusable = false,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                ),
+                onDismissRequest = onDismissRequest
+            ) {
+                list.forEach { plant ->
+                    DropdownMenuItem(onClick = {
+                        setValue(
+                            TextFieldValue(
+                                plant.toString(),
+                                TextRange(plant.toString().length)
+                            )
+                        )
+                    }) {
+                        Text(text = plant.toString())
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        MyPlantDiaryTheme {
+            SpecimenFacts(plants = ArrayList<Plant>())
+        }
     }
 }
